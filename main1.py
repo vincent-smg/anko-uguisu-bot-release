f765c1b5824abadcad8d41280630042cd6b82e94
vincent-smg <godieloserr@gmail.com>
2026-06-15 16:50:50 +0400

    Update main1.py

--- main1.py
+++ main1.py
@@ -16,8 +16,29 @@ from datetime import datetime, timezone
 import urllib.parse
 import json
 from collections import defaultdict
+import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
- 
+
+
+
+
+
+
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
+
+threading.Thread(target=run_server).start()
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
  

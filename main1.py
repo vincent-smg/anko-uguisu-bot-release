1025cf53702cea65afc21b95eaeb377363246665
vincent-smg <godieloserr@gmail.com>
2026-06-15 17:12:32 +0400

    Update main1.py

--- main1.py
+++ main1.py
@@ -21,24 +21,24 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
 
-
-
-
-
-
-
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Bot is running")
+        self.wfile.write(b"Bot is alive")
 
 def run_server():
     port = int(os.environ.get("PORT", 10000))
     server = HTTPServer(("0.0.0.0", port), Handler)
     server.serve_forever()
 
-threading.Thread(target=run_server).start()
+threading.Thread(target=run_server, daemon=True).start()
+
+
+
+
+
+
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
  
